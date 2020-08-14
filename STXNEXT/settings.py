a11d0ffd71face74064828d66d9844d67b577e8b
TomWalcz <tomasz.walczak90@gmail.com>
2020-08-14 18:59:32 +0200

    Uporzadkowanie struktury, naprawa wyszukiwarki

--- STXNEXT/settings.py
+++ STXNEXT/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'books.apps.BooksConfig',
     'api.apps.ApiConfig',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
--- books/views.py
+++ books/views.py
@@ -2,31 +2,19 @@ from rest_framework import mixins
 from rest_framework import generics
 from books.models import Book
 from books.serializers import BookSerializer
-from rest_framework import filters
-from django_filters import rest_framework as filters
+# import django_filters.rest_framework
+# import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-class BookList( mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
-
-    queryset = Book.objects.all()
+class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-        
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    queryset = Book.objects.all()
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'authors']
+
 
 class BookDetail(generics.RetrieveAPIView):
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    # def put(self, request, *args, **kwargs):
-    #     return self.update(request, *args, **kwargs)
-
-    # def delete(self, request, *args, **kwargs):
-    #     return self.destroy(request, *args, **kwargs)
